11abbd3f646829ee5c956816a850a296c869cf66
RodrgioNPLAY <rodrigo.nhs12@outlook.com>
2021-08-25 00:56:14 -0300

    Cabeçalho

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html>
 
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Doceria</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+    <link rel="stylesheet" href="css/styles.css">
+
+
+</head>
+
+<body>
+    <article>
+        <header>
+            <div>
+                <ul>
+                    <li><a href="#index.php">Home</a></li>
+                    <li><a href="#">Quem somos</a></li>
+                    <li><a href="#">Chef</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+</body>
+
+</html>
+
+<?php
 ?>
